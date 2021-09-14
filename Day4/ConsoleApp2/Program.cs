832002a5ab897b93b83866d688b6f636022245e5
Shiya-Roby <55143677+Shiya-Roby@users.noreply.github.com>
2021-09-14 07:35:16 -0400

    Add files via upload

---
+++ Day4/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_demo
+{
+    interface IUser
+    {
+        int a { get; set; }
+        void work();
+    }
+    interface A: IUser { }
+    class User: IUser
+    {
+        public int a { get => a;
+            set => a = value;
+
+        }
+        public void work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
